9ce29278dc24a6e576cfe88964fb8a7b4c9df0a8
Ivan Denisenko <57327265+IvanDenisenko@users.noreply.github.com>
2021-03-14 12:42:17 +0300

    init: new brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+module.exports = function check(str, brack){
+  a = str.split("");
+  for (let i = 0; i< brack[0].length; i++){
+
+  if(brack[0][i].includes(a[i]) == true){
+  return true;}
+  else return false;
+  }
+  }
